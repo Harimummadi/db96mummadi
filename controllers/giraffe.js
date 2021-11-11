1cbf4b31ddd98097d39a8d99e1e340f18c3caef4
Harimummadi <mummadiharihara9@gmail.com>
2021-11-10 23:23:03 -0600

    modified giraffe.js in controller

--- controllers/giraffe.js
+++ controllers/giraffe.js
@@ -1,28 +1,62 @@
 var giraffe = require('../models/giraffe');
-// List of all giraffes
-exports.giraffe_list = async function(req, res) {
-    try{
-    thegiraffe = await giraffe.find();
-    res.send(thegiraffe);
+
+/// List of all giraffe 
+exports.giraffe_list = async function (req, res) {
+    try {
+        thegiraffe = await giraffe.find();
+        res.send(thegiraffe);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };
+};
+
+// VIEWS
+// Handle a show all view
+exports.giraffe_view_all_Page = async function (req, res) {
+    try {
+        thegiraffe = await giraffe.find();
+        res.render('giraffe', { title: 'giraffe Search Results', results: thegiraffe });
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`)
+        
+    }
+};
+
 // for a specific giraffe.
-exports.giraffe_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: giraffe detail: ' + req.params.id);
+exports.giraffe_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: giraffe detail: ' + req.params.id);
 };
-// Handle giraffe create on POST.
-exports.giraffe_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: giraffe create POST');
+
+// Handle giraffe create on POST. 
+exports.giraffe_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new giraffe();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.brand = req.body.brand; 
+    document.flavour = req.body.flavour; 
+    document.cost = req.body.cost;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
+
 // Handle giraffe delete form on DELETE.
-exports.giraffe_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: giraffe delete DELETE ' + req.params.id);
+exports.giraffe_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: giraffe delete DELETE ' + req.params.id);
 };
 // Handle giraffe update form on PUT.
-exports.giraffe_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: giraffe update PUT' + req.params.id);
+exports.giraffe_update_put = function (req, res) {
+    res.send('NOT IMPLEMENTED: giraffe update PUT' + req.params.id);
 };
--- routes/giraffe.js
+++ routes/giraffe.js
@@ -1,6 +1,11 @@
 
 var express = require('express');
+const giraffe_controlers= require('../controllers/giraffe');
 var router = express.Router();
+
+/* GET giraffe */ 
+router.get('/', giraffe_controlers.giraffe_view_all_Page );
+module.exports = router;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
