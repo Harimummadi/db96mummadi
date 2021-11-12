c8f1dbf0c976c88a898522d30a7d6394ab812466
Harimummadi <mummadiharihara9@gmail.com>
2021-11-12 13:20:42 -0600

    Lab 12 completed

--- controllers/giraffe.js
+++ controllers/giraffe.js
@@ -38,7 +38,7 @@ exports.giraffe_create_post = async function (req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"giraffe_type":"goat", "cost":12, "size":"large"}
     document.brand = req.body.brand; 
     document.flavour = req.body.flavour; 
     document.cost = req.body.cost;
@@ -56,7 +56,35 @@ exports.giraffe_create_post = async function (req, res) {
 exports.giraffe_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: giraffe delete DELETE ' + req.params.id);
 };
-// Handle giraffe update form on PUT.
-exports.giraffe_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: giraffe update PUT' + req.params.id);
+// Handle giraffe update form on PUT. 
+exports.giraffe_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await giraffe.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.giraffe_type)  
+               toUpdate.brand = req.body.brand; 
+        if(req.body.size) toUpdate.size = req.body.size; 
+        if(req.body.cost) toUpdate.cost = req.body.cost; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
+
+// for a specific giraffe. 
+exports.giraffe_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await giraffe.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 };
